5206cf738dbd8deba90ee07d73eb43ca040edce8
duleweb <44978033+duleweb@users.noreply.github.com>
2022-05-09 10:11:58 +0200

    Add files via upload

---
+++ ex/js/2.js
@@ -0,0 +1,7 @@
+$( "#header > section.header-top-content > div > div > div > div.col-sm-12 > div.contact-info" ).html(`<img src="http://localhost/portf/res/logoipsum-logo-7.svg">`);
+$("div.footer-message").html("Your company name");
+$("span.footer-logo-title").html("Company");
+$("#about-us > section > div > div > div.col-md-12 > div > h2").text("Welcome...");
+$("#about-us > section > div > div > div.col-md-12 > div > h4").text("Why us?");
+
+$("body").append(`<a id="mde_by" href="https://www.upwork.com/fl/~016d129301f3fc6c77" target="_blank"><img style="width: 175px;" src="http://localhost/portf/res/made_by.png"/>`);
